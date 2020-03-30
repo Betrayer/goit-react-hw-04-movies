c0ef641b04df0616bc748f4b1663bc77ff745050
Betrayer <StarDestroyer@ukr.net>
2020-03-30 15:52:42 +0300

    added lazy

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,27 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import HomePage from "./pages/HomePage";
-import MovieDetailsPage from "./pages/MovieDetailsPage";
-import MoviesPage from "./pages/MoviesPage";
-import Navigation from "./navigation/Navigation";
 import { Switch, Route, Redirect } from "react-router-dom";
+const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage"));
+const MoviesPage = lazy(() => import("./pages/MoviesPage"));
+const Navigation = lazy(() => import("./navigation/Navigation"));
 
 const App = () => (
   <>
-    <Navigation />
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/movies/:movieId" component={MovieDetailsPage} />
-      <Route path="/movies" component={MoviesPage} />
-      <Redirect to="/" />
-    </Switch>
+    <Suspense
+      fallback={
+        <div>
+          <p>Please wait...</p>
+        </div>
+      }
+    >
+      <Navigation />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/movies/:movieId" component={MovieDetailsPage} />
+        <Route path="/movies" component={MoviesPage} />
+        <Redirect to="/" />
+      </Switch>
+    </Suspense>
   </>
 );
 
