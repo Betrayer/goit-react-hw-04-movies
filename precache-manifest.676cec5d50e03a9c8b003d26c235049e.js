8531a943b6c01b661d0bd0e0b58f9b0767372a8d
Betrayer <StarDestroyer@ukr.net>
2020-03-30 16:06:24 +0300

    Updates

---
+++ precache-manifest.676cec5d50e03a9c8b003d26c235049e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf17cc373dc3a379ade295f0642f4451",
+    "url": "/goit-react-hw-04-movies/index.html"
+  },
+  {
+    "revision": "d43fab122d791f713018",
+    "url": "/goit-react-hw-04-movies/static/css/3.07f54d7b.chunk.css"
+  },
+  {
+    "revision": "33dd26ff9e737814ed7d",
+    "url": "/goit-react-hw-04-movies/static/css/4.74454472.chunk.css"
+  },
+  {
+    "revision": "52ec0be883f6911ede33",
+    "url": "/goit-react-hw-04-movies/static/css/5.145daaa7.chunk.css"
+  },
+  {
+    "revision": "c835a354496c8c6f79ed",
+    "url": "/goit-react-hw-04-movies/static/css/main.aa64621a.chunk.css"
+  },
+  {
+    "revision": "a7207361575914f1e3d2",
+    "url": "/goit-react-hw-04-movies/static/js/2.e5482c69.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-04-movies/static/js/2.e5482c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d43fab122d791f713018",
+    "url": "/goit-react-hw-04-movies/static/js/3.50c0a1fd.chunk.js"
+  },
+  {
+    "revision": "33dd26ff9e737814ed7d",
+    "url": "/goit-react-hw-04-movies/static/js/4.c6b5a289.chunk.js"
+  },
+  {
+    "revision": "52ec0be883f6911ede33",
+    "url": "/goit-react-hw-04-movies/static/js/5.70779c5d.chunk.js"
+  },
+  {
+    "revision": "c835a354496c8c6f79ed",
+    "url": "/goit-react-hw-04-movies/static/js/main.ca629798.chunk.js"
+  },
+  {
+    "revision": "0704b343b94de5e54f7d",
+    "url": "/goit-react-hw-04-movies/static/js/runtime-main.42b10450.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7773062cde55831a888934503b5d43be.js"
+  "/goit-react-hw-04-movies/precache-manifest.676cec5d50e03a9c8b003d26c235049e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/goit-react-hw-04-movies/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
