1550fa4ad8a1c3ef38c369ac22809d03fd91a07a
Betrayer <StarDestroyer@ukr.net>
2020-03-30 16:00:53 +0300

    Updates

---
+++ precache-manifest.7773062cde55831a888934503b5d43be.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ee346f5f2bdd8a340c22e5448cf0ade",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d43fab122d791f713018",
+    "url": "/static/css/3.07f54d7b.chunk.css"
+  },
+  {
+    "revision": "33dd26ff9e737814ed7d",
+    "url": "/static/css/4.74454472.chunk.css"
+  },
+  {
+    "revision": "52ec0be883f6911ede33",
+    "url": "/static/css/5.145daaa7.chunk.css"
+  },
+  {
+    "revision": "c835a354496c8c6f79ed",
+    "url": "/static/css/main.aa64621a.chunk.css"
+  },
+  {
+    "revision": "a7207361575914f1e3d2",
+    "url": "/static/js/2.e5482c69.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e5482c69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d43fab122d791f713018",
+    "url": "/static/js/3.50c0a1fd.chunk.js"
+  },
+  {
+    "revision": "33dd26ff9e737814ed7d",
+    "url": "/static/js/4.c6b5a289.chunk.js"
+  },
+  {
+    "revision": "52ec0be883f6911ede33",
+    "url": "/static/js/5.70779c5d.chunk.js"
+  },
+  {
+    "revision": "c835a354496c8c6f79ed",
+    "url": "/static/js/main.ca629798.chunk.js"
+  },
+  {
+    "revision": "52f8305472832e695508",
+    "url": "/static/js/runtime-main.ec75f7e0.js"
+  }
+]);
---
+++ static/js/5.70779c5d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-04-movies"]=this["webpackJsonpgoit-react-hw-04-movies"]||[]).push([[5],{83:function(e,a,t){e.exports={navigationWrapper:"navigation_navigationWrapper__gsT_j"}},84:function(e,a,t){"use strict";t.r(a);var n=t(0),i=t.n(n),o=t(11),r=t(83),c=t.n(r);a.default=function(){return i.a.createElement("div",{className:c.a.navigationWrapper},i.a.createElement(o.c,{exact:!0,to:"/"},"Home"),i.a.createElement(o.c,{to:"/movies"},"Movies"))}}}]);
+//# sourceMappingURL=5.70779c5d.chunk.js.map
